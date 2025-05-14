f48bbedea253b6144215f9fa5dfc0b9503940717
Gleb <dumanskiygleb@gmail.com>
2025-05-14 19:00:55 +0200

    Initial commit

---
+++ src/main/java/sprout/n/bloom/greenshop_application/GreenshopApplication.java
@@ -0,0 +1,13 @@
+package sprout.n.bloom.greenshop_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenshopApplication.class, args);
+	}
+
+}
